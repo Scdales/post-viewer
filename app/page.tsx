bba6465b7d661527c8fcb463747df9ca3bf2d932
Scdales <28219661+Scdales@users.noreply.github.com>
2024-10-29 20:48:16 +0000

    add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import {POSTS_URL} from "@/lib/constants/endpoints";
-import {TPostsResponse} from "@/lib/types/posts";
-import PostsLists from "@/lib/components/PostsList";
+import { POSTS_URL } from '@/lib/constants/endpoints'
+import { TPostsResponse } from '@/lib/types/posts'
+import PostsLists from '@/lib/components/PostsList'
 
 const INITIAL_POSTS_URL = POSTS_URL + '&limit=20'
 
 export default async function Home() {
-
   const postsReq = await fetch(INITIAL_POSTS_URL)
   const postsData: TPostsResponse = await postsReq.json()
 
@@ -13,5 +12,5 @@ export default async function Home() {
     <div className="items-center justify-items-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <PostsLists posts={postsData.posts} />
     </div>
-  );
+  )
 }
--- lib/components/PostsList/PostsLists.tsx
+++ lib/components/PostsList/PostsLists.tsx
@@ -1,7 +1,13 @@
-import {TPost} from "@/lib/types/posts";
+import { TPost } from '@/lib/types/posts'
 
-const PostsLists = ({ posts }: {posts: TPost[] }) => {
-  return <div>{posts.map((post) => <div>{JSON.stringify(post, null, 4)}</div>)}</div>
+const PostsLists = ({ posts }: { posts: TPost[] }) => {
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>{JSON.stringify(post, null, 4)}</div>
+      ))}
+    </div>
+  )
 }
 
 export default PostsLists
