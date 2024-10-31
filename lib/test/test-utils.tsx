e7b3303355a226eaf0bde8bf29230b58a1bb6351
Scdales <28219661+Scdales@users.noreply.github.com>
2024-10-31 21:32:11 +0000

    jest and testing-library setup

---
+++ lib/test/test-utils.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import StoreProvider from "@/lib/components/StoreProvider";
+import '@testing-library/jest-dom'
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider posts={[]}>
+      {children}
+    </StoreProvider>
+  )
+}
+
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) => render(ui, { wrapper: AllTheProviders, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render }
